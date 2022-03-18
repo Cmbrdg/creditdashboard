a75100b8ffb9aec574c10622550dfebe506a52e8
Cmbrdg <101858072+Cmbrdg@users.noreply.github.com>
2022-03-18 07:39:01 +0100

    Update app4.py

--- app4.py
+++ app4.py
@@ -27,7 +27,6 @@ def predict_species(iris: IrisSpecies):
                                                     data['AMT_ANNUITY'])
     return {'prediction': prediction,'probability': probability}
 
-
 # 4. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
